8d0c9a4fa7f014ec19ac681b7c6678388b2eacad
ys0222 <91381230+ys0222@users.noreply.github.com>
2022-01-31 18:21:18 +0900

    Update 0131.py

--- 2022-01/0131.py
+++ 2022-01/0131.py
@@ -24,3 +24,47 @@ print(english_dict[a])
 
 //문제에서 공백 딕셔너리로 만들라해서..
 
+#연습문제 p276~
+1.
+numbers = [] 
+count = 0
+
+while count < 5 :
+    number = int(input("정수를 입력하시오 : "))
+    numbers.append(number)
+    count = count +1
+
+avg = sum(numbers) / len(numbers)
+print("평균 :" , avg)
+>>>
+정수를 입력하시오 : 10
+정수를 입력하시오 : 20
+정수를 입력하시오 : 30
+정수를 입력하시오 : 40
+정수를 입력하시오 : 50
+평균 : 30.0
+         
+2.
+import random
+
+count = 0
+n = [0, 0, 0, 0, 0, 0]
+
+while count < 1000:
+    a = random.randint(0,5)
+    n[a] = n[a] +1
+    count = count + 1 
+     
+print("주사위 1인 경우는 : ", n[0])
+print("주사위 2인 경우는 : ", n[1])
+print("주사위 3인 경우는 : ", n[2])
+print("주사위 4인 경우는 : ", n[3])
+print("주사위 5인 경우는 : ", n[4])
+print("주사위 6인 경우는 : ", n[5])
+>>>
+주사위 1인 경우는 :  189
+주사위 2인 경우는 :  178
+주사위 3인 경우는 :  146
+주사위 4인 경우는 :  175
+주사위 5인 경우는 :  152
+주사위 6인 경우는 :  160
