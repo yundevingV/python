8e89ccf5041e3c4bc535c96e757d7aff074b683f
ys0222 <91381230+ys0222@users.noreply.github.com>
2022-01-31 16:44:18 +0900

    Create 0131.py

---
+++ 2022-01/0131.py
@@ -0,0 +1,26 @@
+#편의점 재고관리
+
+items = {"커피음료" : 7, "펜" : 3, "종이컵" : 2, 
+         "우유" : 1, "콜라" : 4, "책" : 5}
+
+a = input("물건의 이름을 입력하시오 : ")
+
+print(items[a])
+>>>
+물건의 이름을 입력하시오 : 콜라
+4
+
+#영한사전
+
+english_dict = {}
+
+english_dict["one"] = '하나'
+english_dict["two"] = '둘'
+english_dict["three"] = '셋'
+
+a = input("단어를 입력하시오 : ")
+
+print(english_dict[a])
+
+//문제에서 공백 딕셔너리로 만들라해서..
+
